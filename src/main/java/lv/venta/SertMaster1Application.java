ee6292aa0283a1e1700201db41c19818e59c38b8
krisnawa00 <kristersrudzitiss@gmail.com>
2025-06-10 21:46:10 +0300

    initial commit

---
+++ src/main/java/lv/venta/SertMaster1Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SertMaster1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SertMaster1Application.class, args);
+	}
+
+}
