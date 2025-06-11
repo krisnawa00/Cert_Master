441898b79aa924b5896ad01e3cb9a72efef6c805
Karsonics <karsonics@gmail.com>
2025-06-11 20:18:24 +0300

    Lietotājs

---
+++ src/main/java/lv/venta/model/Lietotajs.java
@@ -0,0 +1,53 @@
+package lv.venta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "Lietotajs")
+
+public class Lietotajs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "L_ID")
+    private long lId;
+
+
+    @NotNull
+    @Column(name = "Parole")
+    private String parole;
+
+    @NotNull
+    @Email
+    @Column(name = "E_pasts")
+    private String ePasts;
+
+
+    @Builder
+    public Lietotajs(String parole, String ePasts) {
+        this.parole = parole;
+        this.ePasts = ePasts;
+    }
+
+
+
+
+}
