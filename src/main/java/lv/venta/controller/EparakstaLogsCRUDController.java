2f0f5e970644bd770ad3b005af07c99a3791f515
Karsonics <karsonics@gmail.com>
2025-06-15 21:01:12 +0300

    i want to shoot myself

---
+++ src/main/java/lv/venta/controller/EparakstaLogsCRUDController.java
@@ -0,0 +1,65 @@
+package lv.venta.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lv.venta.model.EParakstaLogs;
+import lv.venta.service.impl.EparakstaLogsCRUDService;
+
+
+@Controller
+@RequestMapping("crud/eparakstalogs")
+public class EparakstaLogsCRUDController {
+
+
+    @Autowired
+    private EparakstaLogsCRUDService eparakstaLogsCRUDService; 
+
+    @GetMapping("/eparakstalogs/show/all")
+    public String getAllEparakstaLogs(Model model){
+
+        try {
+            Iterable<EParakstaLogs> eparaksts = eparakstaLogsCRUDService.retrieveAllEParakstaLogs();
+            model.addAttribute("eparaksts", eparaksts);
+            return "eparaksta-logs-page";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error-page";
+        }
+    }  
+    
+    @GetMapping("/eparakstalogs/{id}")
+    public String getEparakstaLogById(@PathVariable("id") int id, Model model) {
+    try {
+        EParakstaLogs eparakstaLogs = eparakstaLogsCRUDService.retrieveEParakstaLogById(id);
+        model.addAttribute("eparakstaLogs", eparakstaLogs);
+        return "one-eparaksta-logs-page";
+    } catch (Exception e) {
+        model.addAttribute("error", e.getMessage());
+        return "error-page";
+    }
+}
+
+    @GetMapping("/eparakstalogs/delete/{id}")
+    public String deleteEparakstaLogs(@PathVariable("id") int id,Model model){
+        try {
+            eparakstaLogsCRUDService.deleteMacibuRezultatiById(id);
+            model.addAttribute("rezultati", eparakstaLogsCRUDService.retrieveAllEParakstaLogs());
+            return "eparaksta-logs-page";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error-page";
+        }
+
+
+
+    }
+
+
+
+
+}
